98608784523cf416684a5b8c276d97d6fddd8674
Ilya <ilia.nikulin@gmail.com>
2018-10-07 13:58:56 +0300

    checkout confirmation

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -37,36 +37,36 @@
 
                     <div class="form-group">
                         <label for="email">Email Address</label>
-                        <input type="email" class="form-control" id="email" name="email" value="">
+                        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
                     </div>
                     <div class="form-group">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" value="">
+                        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
                     </div>
                     <div class="form-group">
                         <label for="address">Address</label>
-                        <input type="text" class="form-control" id="address" name="address" value="">
+                        <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}" required>
                     </div>
 
                     <div class="half-form">
                         <div class="form-group">
                             <label for="city">City</label>
-                            <input type="text" class="form-control" id="city" name="city" value="">
+                            <input type="text" class="form-control" id="city" name="city" value="{{ old('city') }}" required>
                         </div>
                         <div class="form-group">
                             <label for="province">Province</label>
-                            <input type="text" class="form-control" id="province" name="province" value="">
+                            <input type="text" class="form-control" id="province" name="province" value="{{ old('province') }}" required>
                         </div>
                     </div> <!-- end half-form -->
 
                     <div class="half-form">
                         <div class="form-group">
                             <label for="postalcode">Postal Code</label>
-                            <input type="text" class="form-control" id="postalcode" name="postalcode" value="">
+                            <input type="text" class="form-control" id="postalcode" name="postalcode" value="{{ old('postalcode') }}" required>
                         </div>
                         <div class="form-group">
                             <label for="phone">Phone</label>
-                            <input type="text" class="form-control" id="phone" name="phone" value="">
+                            <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone') }}" required>
                         </div>
                     </div> <!-- end half-form -->
 
@@ -113,7 +113,7 @@
 
                     <div class="spacer"></div>
 
-                    <button type="submit" class="button-primary full-width">Complete Order</button>
+                    <button type="submit" id="complete-order" class="button-primary full-width">Complete Order</button>
 
 
                 </form>
@@ -220,6 +220,9 @@
             form.addEventListener('submit', function(event) {
                 event.preventDefault();
 
+                // Disable the submit button to prevent repeated clicks
+                document.getElementById('complete-order').disabled = true;
+
                 var options = {
                     name: document.getElementById('name_on_card').value,
                     address_line1: document.getElementById('address').value,
@@ -233,6 +236,9 @@
                         // Inform the user if there was an error.
                         var errorElement = document.getElementById('card-errors');
                         errorElement.textContent = result.error.message;
+
+                        // Enable the submit button
+                        document.getElementById('complete-order').disabled = false;
                     } else {
                         // Send the token to your server.
                         stripeTokenHandler(result.token);
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::post('/saveForLater/switchToCart/{product}', 'SaveForLaterController@swit
 Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
 Route::post('/checkout', 'CheckoutController@store')->name('checkout.store');
 
-Route::view('/thankyou', 'thankyou');
+Route::get('/thankyou', 'ConfirmationController@index')->name('confirmation.index');
