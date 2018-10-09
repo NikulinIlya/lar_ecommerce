5c7162af94f45806c4c0fbb85a0a7cda7516ca11
Ilya <ilia.nikulin@gmail.com>
2018-10-09 16:27:26 +0300

    coupons controller

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ Route::post('/cart/switchToSaveForLater/{product}', 'CartController@switchToSave
 Route::delete('/saveForLater/{product}', 'SaveForLaterController@destroy')->name('saveForLater.destroy');
 Route::post('/saveForLater/switchToCart/{product}', 'SaveForLaterController@switchToCart')->name('saveForLater.switchToCart');
 
-//Route::get('empty', function() {
-//    Cart::instance('saveForLater')->destroy();
-//});
+Route::post('/coupon', 'CouponsController@store')->name('coupon.store');
+Route::delete('/coupon', 'CouponsController@destroy')->name('coupon.destroy');
 
 Route::get('/checkout', 'CheckoutController@index')->name('checkout.index');
 Route::post('/checkout', 'CheckoutController@store')->name('checkout.store');
