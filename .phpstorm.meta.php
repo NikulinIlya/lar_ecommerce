c521b370431424279cdfa728b6b7526344247f9a
Ilya <ilia.nikulin@gmail.com>
2018-10-09 16:43:37 +0300

    deps upgrade

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-10-06 19:37:31.
+    * Generated on 2018-10-09 13:42:39.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
