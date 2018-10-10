b2976bded175d0f0152fedb599aad79a8b64c7f8
Ilya <ilia.nikulin@gmail.com>
2018-10-10 12:20:26 +0300

    voyager menus

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -20,23 +20,13 @@
         <header class="with-background">
             <div class="top-nav container">
                 <div class="logo">Laravel Ecommerce</div>
-                <ul>
-                    <li><a href="{{ route('shop.index') }}">Shop</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li>
-                        <a href="{{ route('cart.index') }}">Cart <span class="cart-count">
-                            @if(Cart::instance('default')->count() > 0)
-                                <span>{{ Cart::instance('default')->count() }}</span></span>
-                            @endif
-                        </a>
-                    </li>
-                </ul>
+                {{ menu('main', 'partials.menus.main') }}
             </div> <!-- end top-nav -->
             <div class="hero container">
                 <div class="hero-copy">
                     <h1>Laravel Ecommerce</h1>
                     <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
+
                     <div class="hero-buttons">
                         <a href="#" class="button button-white">Blog Post</a>
                         <a href="https://github.com/NikulinIlya/lar_ecommerce" class="button button-white">GitHub</a>
