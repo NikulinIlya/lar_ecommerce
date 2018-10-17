deb316c3ba4d4e5d66557353396aa38a361e6ec8
Ilya <ilia.nikulin@gmail.com>
2018-10-17 16:04:52 +0300

    products quantity control

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -58,6 +58,7 @@
         <div class="product-section-information">
             <h1 class="product-section-title">{{ $product->name }}</h1>
             <div class="product-section-subtitle">{{ $product->details }}</div>
+            <div>{!! $stockLevel !!}</div>
             <div class="product-section-price">{{ $product->presentPrice() }}</div>
 
             <p>
@@ -66,14 +67,15 @@
 
             <p>&nbsp;</p>
 
-            {{--<a href="#" class="button">Add to Cart</a>--}}
-            <form action="{{ route('cart.store') }}" method="POST">
-                {{ csrf_field() }}
-                <input type="hidden" name="id" value="{{ $product->id }}">
-                <input type="hidden" name="name" value="{{ $product->name }}">
-                <input type="hidden" name="price" value="{{ $product->price }}">
-                <button type="submit" class="button button-plain">Add to Cart</button>
-            </form>
+            @if ($product->quantity > 0)
+                <form action="{{ route('cart.store') }}" method="POST">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="id" value="{{ $product->id }}">
+                    <input type="hidden" name="name" value="{{ $product->name }}">
+                    <input type="hidden" name="price" value="{{ $product->price }}">
+                    <button type="submit" class="button button-plain">Add to Cart</button>
+                </form>
+            @endif
         </div>
     </div> <!-- end product-section -->
 
@@ -105,7 +107,7 @@
 
 @section('extra-js')
     <!-- Include AlgoliaSearch JS Client and autocomplete.js library -->
-    <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
     <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
     <script src="{{ asset('js/algolia.js') }}"></script>
 @endsection
